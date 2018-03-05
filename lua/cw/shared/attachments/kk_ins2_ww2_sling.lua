e0f6488c2678185a082cc891a1773b197772a456
Ľubor Slušný <lubor.slusny@gmail.com>
2018-03-05 01:25:06 +0100

    updated sling to use SWEP.SlingBGs if present

--- lua/cw/shared/attachments/kk_ins2_ww2_sling.lua
+++ lua/cw/shared/attachments/kk_ins2_ww2_sling.lua
@@ -13,6 +13,18 @@ if CLIENT then
 	att.description = {
 		-- [1] = {t = "Boosts deploy speed of weapon that you switch to.", c = CustomizableWeaponry.textColors.VPOSITIVE},
 	}
+
+	function att:attachFunc()
+		if not self.SlingBGs then return end
+
+		self:setBodygroup(self.SlingBGs.main, self.SlingBGs.sling)
+	end
+
+	function att:detachFunc()
+		if not self.SlingBGs then return end
+
+		self:setBodygroup(self.SlingBGs.main, self.SlingBGs.none)
+	end
 end
 
 CustomizableWeaponry:registerAttachment(att)
